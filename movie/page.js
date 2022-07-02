a266655c1cef2429d0d1a2bc9b7f5a3bccf1c027
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-02 15:51:16 -0400

    Create page.js

---
+++ movie/page.js
@@ -0,0 +1,34 @@
+const fUtil = require("../fileUtil");
+/**
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ * @param {import("url").UrlWithParsedQuery} url
+ * @returns {boolean}
+ */
+module.exports = function (req, res, url) {
+	if (req.method != "GET") return;
+	const query = url.query;
+
+	var params;
+	switch (url.pathname) {
+		case "/go_full": {
+			break;
+		}
+
+		default:
+			return;
+	}
+	res.setHeader("Content-Type", "text/html; charset=UTF-8");
+	Object.assign(query);
+	res.end(`<html>
+	<head>
+		<script>
+			function genorateId() { 
+				window.location = '${query.return}?newId=${query.movieId && query.movieId.startsWith("m") ? query.movieId : `m-${fUtil[query.noAutosave ? "getNextFileId" : "fillNextFileId"]("movie-", ".xml")}`}&tray=${query.theme}'; 
+			}
+		</script>
+	</head>
+	<body onload="genorateId()"></body>
+</html>`)
+	return true;
+};
