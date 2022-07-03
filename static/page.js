b0836070d232b2cd0a20544588ee17acc3c1a466
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-03 00:48:18 -0400

    Update page.js

--- static/page.js
+++ static/page.js
@@ -60,22 +60,6 @@ module.exports = function (req, res, url) {
 			};
 			break;
 		}
-			
-		case '/player': {
-			title = 'Video Player';
-			attrs = {
-				data: process.env.SWF_URL + '/player.swf',
-				type: 'application/x-shockwave-flash', width: '100%', height: '100%',
-			};
-			params = {
-				flashvars: {
-					'apiserver': '/', 'storePath': process.env.STORE_URL + '/<store>', 'ut': 60,
-					'autostart': 1, 'isWide': 1, 'clientThemePath': process.env.CLIENT_URL + '/<client_theme>',
-				},
-				allowScriptAccess: 'always',
-			};
-			break;
-		}
 
 		default:
 			return;
