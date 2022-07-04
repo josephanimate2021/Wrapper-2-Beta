ad19c884a0f1b323e943b93b452998abda6c75ac
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-03 23:20:46 -0400

    Update create.js

--- movie/create.js
+++ movie/create.js
@@ -1,13 +1,10 @@
 module.exports = function (req, res, url) {
-	switch (req.method) {
-		case 'GET': {
-      const match = req.url.match(/\/go\/create\/$/);
-      if (!match) return;
+	if (req.method != 'GET') return;
+	const match = req.url.match(/\/go\/create\/$/);
+	if (!match) return;
       
-      res.statusCode = 302;
-      res.setHeader('Location', '/go_full?action=create&tray=retro&usePreviewWindowInStudio=true');
-      res.end();
-			return true;
-    }
-  }
+	res.statusCode = 302;
+	res.setHeader('Location', '/go_full?action=create&tray=retro&usePreviewWindowInStudio=true');
+	res.end();
+	return true;
 }
