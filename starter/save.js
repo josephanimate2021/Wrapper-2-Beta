d4d67d891fa692fb1c9843318933baec778728b6
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-03 22:33:26 -0400

    Update save.js

--- starter/save.js
+++ starter/save.js
@@ -9,11 +9,19 @@ const http = require("http");
  * @returns {boolean}
  */
 module.exports = function (req, res, url) {
-	if (req.method != "POST" || (url.path != "/goapi/saveTemplate/")) return;
-	loadPost(req, res).then(([data, mId]) => {
-		var body = Buffer.from(data.body_zip, "base64");
-		var thumb = data.thumbnail_large && Buffer.from(data.thumbnail_large, "base64");
-		starter.save(body, thumb).then((nId) => res.end("0" + nId));
-	});
-	return true;
+	if (req.method != "POST") return;
+	switch (url.path) {
+		case "/goapi/saveTemplate/": {
+			loadPost(req, res).then(([data, mId]) => {
+				var body = Buffer.from(data.body_zip, "base64");
+				var thumb = data.thumbnail_large && Buffer.from(data.thumbnail_large, "base64");
+				starter.save(body, thumb).then((nId) => res.end("0" + nId));
+			});
+			return true;
+		}
+		case "/goapi/deleteUserTemplate/": {
+			starter.delete();
+			break;
+		}
+	}
 };
