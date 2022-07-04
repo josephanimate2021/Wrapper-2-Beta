2a670b9a102a30b61b5863f3a77f6a1e5f1dbb97
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-03 22:45:46 -0400

    Update save.js

--- starter/save.js
+++ starter/save.js
@@ -12,10 +12,10 @@ module.exports = function (req, res, url) {
 	if (req.method != "POST") return;
 	switch (url.path) {
 		case "/goapi/saveTemplate/": {
-			loadPost(req, res).then(([data, mId]) => {
-				var body = Buffer.from(data.body_zip, "base64");
-				var thumb = data.thumbnail_large && Buffer.from(data.thumbnail_large, "base64");
-				starter.save(body, thumb).then((nId) => res.end("0" + nId));
+			loadPost(req, res).then(data => {
+				var thumb = data.thumbnail_large && Buffer.from(data.thumbnail_large, 'base64');
+				var body = Buffer.from(data.body_zip, 'base64');
+				starter.save(body, thumb).then(nId => res.end('0' + nId));
 			});
 			return true;
 		}
