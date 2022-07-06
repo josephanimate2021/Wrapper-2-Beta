ed2f2c153b7b0178ec1bec91333176f5b95bcaf7
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-06 10:22:18 -0400

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const stl = require('./static/load');
 const stp = require('./static/page');
 const asl = require('./asset/load');
 const asL = require('./asset/list');
-const Asl = require('./Asset/list');
 const ast = require('./asset/thmb');
 const mtp = require('./movie/page');
 const Mtp = require('./movie/remote');
@@ -34,7 +33,6 @@ const url = require('url');
 const functions = [
 	mvL,
 	svs,
-	Asl,
 	svt,
 	pmc,
 	asl,
